d7cc149c449a50e025e593abfa4e69028d59e09c
Axel COCHELIN <ax.cochelin@gmail.com>
2021-02-02 16:59:57 +0100

    Structuration des dossiers backend + imports bcrypt, body-parser et jwt

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
 // Imports
 const express = require("express");
+const bodyParser = require("body-parser");
+const apiRouter = require("./routes/users").router;
 
 // Instantiate server
 const server = express();
+
+// Body-parser configuration
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 
 // Configure routes
 server.get("/", (req, res) => {
   res.setHeader("Content-Type", "text/html");
   res.status(200).send("<h1>Bienvenue sur mon server</h1>");
 });
+
+server.use("/api/", apiRouter);
 
 // Launch server
 server.listen(8080, () => {
