4f989561b722ea28b427cb4da545bf7f73171fd2
Axel COCHELIN <ax.cochelin@gmail.com>
2021-02-03 08:05:44 +0100

    Installation du système de filtrage et de vérification des email, password et username length

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,6 +2,10 @@
 let bcrypt = require("bcrypt");
 let jwtUtils = require("../utils/jwt.utils");
 let models = require("../models");
+let asyncLib = require("async");
+
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$/;
 
 //Controllers
 module.exports = {
@@ -16,38 +20,72 @@ module.exports = {
       return res.status(400).json({ error: "missing parameters" });
     }
 
-    //to verify pseudo lenght, email regex, password...
+    if (username.length >= 21 || username.length <= 2) {
+      return res
+        .status(400)
+        .json({ error: "wrong username (must be length 3 - 20)" });
+    }
 
-    models.User.findOne({
-      attributes: ["email"],
-      where: { email: email },
-    })
-      .then((userFound) => {
-        if (!userFound) {
-          bcrypt.hash(password, 5, (err, bcryptedPassword) => {
-            let newUser = models.User.create({
-              email: email,
-              password: bcryptedPassword,
-              username: username,
-              image: image,
-              isAdmin: 0,
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ error: "email is not valid" });
+    }
+
+    if (!passwordRegex.test(password)) {
+      return res.status(400).json({
+        error:
+          "password not valid (must lenght 4 - 8 and include 1 number and 1 uppercase",
+      });
+    }
+
+    asyncLib.waterfall(
+      [
+        (done) => {
+          models.User.findOne({
+            attributes: ["email"],
+            where: { email: email },
+          })
+            .then((userFound) => {
+              done(null, userFound);
             })
-              .then((newUser) => {
-                return res.status(201).json({
-                  userId: newUser.id,
-                });
-              })
-              .catch((err) => {
-                return res.status(500).json({ error: "cannot add user" });
-              });
+            .catch((err) => {
+              return res.status(500).json({ error: "unable to verify user" });
+            });
+        },
+        (userFound, done) => {
+          if (!userFound) {
+            bcrypt.hash(password, 5, (err, bcryptedPassword) => {
+              done(null, userFound, bcryptedPassword);
+            });
+          } else {
+            return res.status(409).json({ error: "user already exist" });
+          }
+        },
+        (userFound, bcryptedPassword, done) => {
+          let newUser = models.User.create({
+            email: email,
+            password: bcryptedPassword,
+            username: username,
+            image: image,
+            isAdmin: 0,
+          })
+            .then((newUser) => {
+              done(newUser);
+            })
+            .catch((err) => {
+              return res.status(500).json({ error: "cannot add user" });
+            });
+        },
+      ],
+      (newUser) => {
+        if (newUser) {
+          return res.status(201).json({
+            userId: newUser.id,
           });
         } else {
-          return res.status(409).json({ error: "user already exist" });
+          return res.status(500).json({ error: "cannot add user" });
         }
-      })
-      .catch((err) => {
-        return res.status(500).json({ error: "unable to verify user" });
-      });
+      }
+    );
   },
 
   login: (req, res) => {
