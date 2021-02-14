8a57b1190e93aa4c26732e41f8fabdfba108440b
Axel COCHELIN <ax.cochelin@gmail.com>
2021-02-14 12:28:52 +0100

    Navbar + import images groupomania

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
   <v-container class="home">
-    <h1>Articles</h1>
-    <v-card v-for="article in articles" :key="article.title"
-      ><router-link :to="{ name: 'one-article', params: { id: article.id } }">
-        <v-card-title>
-          <h3>{{ article.title }}</h3>
-        </v-card-title>
-        <v-card-text>
-          {{ article.content }}
-        </v-card-text>
-      </router-link>
+    <h1 class="display-3 ma-4 d-flex justify-center">Articles</h1>
+    <v-card
+      outlined
+      hover
+      class="ma-4"
+      v-for="article in articles"
+      :key="article.title"
+      :to="{ name: 'one-article', params: { id: article.id } }"
+    >
+      <v-card-title>
+        <h3>{{ article.title }}</h3>
+      </v-card-title>
+      <v-card-text>
+        {{ article.content }}
+      </v-card-text>
     </v-card>
   </v-container>
 </template>
