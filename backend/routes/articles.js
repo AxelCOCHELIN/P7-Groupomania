306979e562d86ea33fadf26fb0eed915a0ab6d52
Axel COCHELIN <ax.cochelin@gmail.com>
2021-02-14 10:22:14 +0100

    V3 from scratch because bugs not understood.

--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -7,7 +7,7 @@ exports.router = (() => {
   let articlesRouter = express.Router();
 
   // Routes assignations
-  articlesRouter.route("/articles/new").post(articlesCtrl.createArticle);
+  articlesRouter.route("/article/new").post(articlesCtrl.createArticle);
   articlesRouter.route("/articles/").get(articlesCtrl.listArticles);
   articlesRouter.route("/article/:id").get(articlesCtrl.oneArticle);
   articlesRouter.route("/article/:id").put(articlesCtrl.modifyArticle);
