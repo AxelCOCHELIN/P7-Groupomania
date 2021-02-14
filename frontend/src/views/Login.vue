07489d68d220dc02083817ecea050207b6484d2f
Axel COCHELIN <ax.cochelin@gmail.com>
2021-02-14 18:05:50 +0100

    ajout de validations du formulaire login

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,23 +1,35 @@
 <template>
   <v-container>
-    <v-form>
+    <v-form v-model="valid">
       <v-text-field
         v-model="loginInfo.email"
         label="Adresse e-mail"
+        :rules="[required('email'), emailFormat()]"
       ></v-text-field
       ><v-text-field
         v-model="loginInfo.password"
         :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
         label="Mot de passe"
+        counter="true"
+        :rules="[
+          required('password'),
+          minLength('password', 8),
+          maxLength('password', 20),
+          passwordFormat(),
+        ]"
         :type="showPassword ? 'text' : 'password'"
         @click:append="showPassword = !showPassword"
       ></v-text-field>
 
-      <v-btn @click="loginUser">Se connecter <v-icon>mdi-login</v-icon></v-btn>
+      <v-btn @click="loginUser" :disabled="!valid"
+        >Se connecter <v-icon>mdi-login</v-icon></v-btn
+      >
     </v-form>
   </v-container>
 </template>
 <script>
+import validations from "@/utils/validations";
+
 export default {
   data() {
     return {
@@ -26,6 +38,7 @@ export default {
         email: "",
         password: "",
       },
+      ...validations,
     };
   },
   methods: {
