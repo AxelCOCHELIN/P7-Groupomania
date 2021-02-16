b367ad6a68da610db5da43db5b12caf3c182c849
Axel COCHELIN <ax.cochelin@gmail.com>
2021-02-17 00:10:30 +0100

    admin user page

--- frontend/src/views/AdminUsers.vue
+++ frontend/src/views/AdminUsers.vue
@@ -4,6 +4,9 @@
     <v-card outlined hover class="ma-4" v-for="user in users" :key="user.id">
       <v-card-title>
         <h3>{{ user.username }}</h3>
+        <v-btn>Afficher</v-btn>
+        <v-btn>Modifier</v-btn>
+        <v-btn @click="deleteUser(user)">Supprimer</v-btn>
       </v-card-title>
     </v-card>
   </v-container>
@@ -17,6 +20,16 @@ export default {
   components: {},
   computed: {
     ...mapState(["users"]),
+  },
+  methods: {
+    deleteUser(user) {
+      let response = confirm(
+        "Êtes vous sur de vouloir supprimer cet utilisateur?"
+      );
+      if (response) {
+        this.$store.dispatch("deleteUser", user);
+      }
+    },
   },
   mounted() {
     this.$store.dispatch("loadUsers");
